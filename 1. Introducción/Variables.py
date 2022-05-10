cbfe2b547c4f45baf39f71da55f7f7d8b64bb4c5
marvalleo <martinezmarce@gmail.com>
2022-05-10 15:21:11 -0400

    Primera subida

---
+++ 1. Introducción/Variables.py
@@ -0,0 +1,12 @@
+nombre = "Valentin Leon"
+numero = 1308
+flotante = 3.14
+
+print(type(nombre))
+print(nombre)
+
+print(type(numero))
+print(numero)
+
+print(type(flotante))
+print(flotante)
---
+++ 3.Strings/Ejercicio1.py
@@ -0,0 +1,13 @@
+cadena = "Te quiero solo como amigo"
+
+print("Imprima los dos primeros caracteres: ", cadena[:2])
+
+print("Imprima los tres últimos caracteres: ", cadena[-3:])
+
+print("Imprima dicha cadena cada dos caracteres: ",cadena[: : 2])
+
+print("Dicha cadena en sentido inverso. Ej.: Si la cadena fuera hola mundo! debe imprimir !odnum aloh: ", cadena[: : -1])
+
+print("Imprima la cadena en un sentido y en sentido inverso. Ej: Si la cadena es “reflejo” imprime reflejoojelfer.")
+
+print(cadena+cadena[: : -1])
