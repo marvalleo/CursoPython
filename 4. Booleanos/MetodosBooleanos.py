a1f70fabb601a59a8d8c1673f1362c2dacf0c9dc
marvalleo <martinezmarce@gmail.com>
2022-05-23 01:26:16 -0400

    Booleanos y condicionales

---
+++ 4. Booleanos/MetodosBooleanos.py
@@ -0,0 +1,9 @@
+cadena = "Estoy mostrando los metodos booleanos para las Strings"
+cadena2 = "CMSJDNCFISDNFISDNCINXICNDIUNSIJND" 
+cadena3 = "cmindcvndsifdnfijofnisncnsmed"
+
+print(cadena.startswith("E")) # entrega True
+print(cadena.startswith("e")) # entrega False
+
+print(cadena2.isupper())
+print(cadena3. islower ())
---
+++ 5. Condicionales/Ejercicio2.py
@@ -0,0 +1,6 @@
+numero = int(input("Ingresa el numero ENTERO: "))
+
+if numero > 0:
+    print("El valor absoluto de {} es: {}".format (numero, numero) )
+else:
+    print("El valor absoluto de {} es: ".format (numero), numero * -1)
