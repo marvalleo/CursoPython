b6d49bd5f9150b701e99ffaf2707478bdf307790
marvalleo <martinezmarce@gmail.com>
2022-05-23 01:35:38 -0400

    modificacion ejercicio2

--- 5. Condicionales/Ejercicio2.py
+++ 5. Condicionales/Ejercicio2.py
@@ -3,4 +3,5 @@ numero = int(input("Ingresa el numero ENTERO: "))
 if numero > 0:
     print("El valor absoluto de {} es: {}".format (numero, numero) )
 else:
-    print("El valor absoluto de {} es: ".format (numero), numero * -1)
+    print("El valor absoluto de {} es: {}".format (numero, abs(numero)))
+#ó  print("El valor absoluto de {} es: ".format (numero), abs(numero))
