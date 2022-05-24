7097999fe92751b8770e31ed7b5def29a18bf313
marvalleo <martinezmarce@gmail.com>
2022-05-24 01:53:39 -0400

    listas

---
+++ 7. Listas/Listas.py
@@ -0,0 +1,8 @@
+lista = []
+
+lista = ['Python' , 120 , 'Nombre' , 4.14 , 6.28]
+
+print((type(lista)))
+print(len(lista))
+lista[0] = 'python'
+print(lista)
---
+++ 7. Listas/MetodosListas3.py
@@ -0,0 +1,12 @@
+lista = ['Python' , 24 , "Rene" , "alexander", 12]
+
+lista[3] = "Alexander"
+print(lista)
+print(lista[3]) 
+
+lista.pop() # Elimina el ultimo valor de una lista
+print(lista)
+lista.pop()
+print (lista)
+lista.remove('Python') # elimina de la lista el valor indicado
+print (lista)
